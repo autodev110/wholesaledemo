ff51f17c2330e3e9eca2cf5b3d15690fa049a605
Dan <dbux777@gmail.com>
2025-10-01 15:57:12 -0400

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = "https://www.simplehomeoffer.org";
+  const siteUrl = "https://simplehomeoffer.org";
 
   return [
     {
